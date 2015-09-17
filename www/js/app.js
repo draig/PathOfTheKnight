61bc83f23fe7d9cc05ad5725b6058f87f4436e75
Andrew <andrew.dashkevich@yahoo.com>
2015-09-17 23:21:04 +0300

    small improvement

--- www/js/app.js
+++ www/js/app.js
@@ -7,6 +7,10 @@ define([
         initialize: function() {
             // Pass in our Router module and call it's initialize function
             this.router.initialize();
+        },
+
+        importCss: function() {
+            $('.app').addClass()
         }
     };
 
@@ -14,9 +18,9 @@ define([
     App.eventAggregator = _.extend({}, Backbone.Events);
 
     App.config = {
-        width: $('body').width(),
-        height: $('body').height(),
-        orientation: $('body').width() > $('body').height() ? 'horizontal' : 'vertical'
+        width: $('.app').width(),
+        height: $('.app').height(),
+        orientation: $('.app').width() > $('.app').height() ? 'horizontal' : 'vertical'
     };
 
     // View.close() event for garbage collection
